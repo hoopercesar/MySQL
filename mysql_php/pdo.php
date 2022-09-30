7d083c2d019cca836fafdfef6db8bf87e6049bea
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-30 09:15:38 -0300

    Add files via upload

--- mysql_php/pdo.php
+++ mysql_php/pdo.php
@@ -9,13 +9,28 @@
 // si se produce la conexión aparece en pantalla conexión exitosa
 // si no aparece el mensaje aparece en pantalla el error. 
 
+print_r($_GET[]);
+
+
 try{
-  $conexion = new PDO('mysql:host=localhost;dbname=prueba_datos', 'root', '');
-  echo 'Conexion exitosa';
+  $conexion = new PDO('mysql:host=localhost;dbname=prueba_consola', 'root', '');
+  echo 'Conexion exitosa'.'<br />';
+
+  // para acceder a la información contenida en la tabla de la base de datos
+  // la tabla es usuarios
+  $resultados = $conexion->query('SELECT*FROM usuarios WHERE id=2');
+
+  // el argumento de query() acepta los comandos de mysql (select nombre from... etc)..
+
+
+  foreach($resultados as $fila) {
+    //print_r($fila);
+    echo $fila['id'].'--'.$fila['nombre']. '--' . $fila['email']. '<br />';
+  }
 } catch(PDOException $e){
   echo 'Error: '.$e->getMessage();
 }
 
-print_r($conexion);
+
 
 ?>
