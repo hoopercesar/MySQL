b2f74535d73eb17abc9af0cfc68e017ef8d1d147
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-02 11:10:08 -0300

    Add files via upload

--- mysqli/leer-datos.php
+++ mysqli/leer-datos.php
@@ -5,16 +5,21 @@ $conexion = new mysqli('localhost', 'root', '', 'prueba_datos');
 if ($conexion->connect_errno !== 0) {
   die('Lo siento. Hubo u$resultado->num_rows');
 } else {
-  $sql = 'SELECT * FROM usuarios';
+  $sql = 'SELECT * FROM datos_usuarios LIMIT 2';
   $resultados = $conexion->query($sql);
 
   if ($resultados->num_rows) {
     //echo $resultados->num_columns;
+    //$uno = $resultados->fetch_assoc()['nombre'];
+    //echo $uno.'<br />';
+    //var_dump($resultados->fetch_assoc()['nombre']);
     //var_dump($resultados->fetch_assoc());
     //var_dump($resultados->fetch_assoc());
 
+    //
+
     while($fila = $resultados->fetch_assoc()) {
-      echo $fila['nombre'];
+      echo $fila['ID'].' -- '.$fila['nombre'].' -- '.$fila['edad'].'<br />';
     }
 
   }else {
